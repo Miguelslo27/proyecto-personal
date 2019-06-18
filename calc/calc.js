ce00d3f0ef28cae04cd4b6dd4f36576fe71350e3
Miguel <miguelmail2006@gmail.com>
2019-06-18 19:09:53 -0300

    feature/calculator-project - Created and implemented Calc class

---
+++ calc/calc.js
@@ -0,0 +1,66 @@
+function Calc() {
+  this.value_1;
+  this.value_2;
+  this.operation;
+  this.result = 0;
+}
+
+Calc.prototype.setVal1 = function (value) {
+  this.value_1 = parseFloat(value);
+}
+
+Calc.prototype.setVal2 = function (value) {
+  this.value_2 = parseFloat(value);
+}
+
+Calc.prototype.setOperation = function (operation) {
+  this.operation = operation;
+}
+
+Calc.prototype.setValue = function (value) {
+  if (this.value_1 == undefined) {
+    this.setVal1(value);
+    this.setResult(this.value_1);
+  } else {
+    this.setVal2(value);
+    this.calcResult();
+  }
+}
+
+Calc.prototype.calcResult = function () {
+  switch(this.operation) {
+    case 'SUM':
+      this.setResult(this.value_1 + this.value_2);
+      break;
+    case 'SUB':
+      this.setResult(this.value_1 - this.value_2);
+      break;
+    case 'DIV':
+      this.setResult(this.value_1 / this.value_2);
+      break;
+    case 'MUL':
+      this.setResult(this.value_1 * this.value_2);
+      break;
+  }
+
+  this.operation = '';
+  this.value_1 = this.result;
+  this.value_2 = 0;
+
+  return this.result;
+}
+
+Calc.prototype.setResult = function (result) {
+  this.result = result;
+}
+
+Calc.prototype.getResult = function () {
+  return this.result;
+}
+
+Calc.prototype.reset = function () {
+  this.value_1 = undefined;
+  this.value_2 = undefined;
+  this.operation = undefined;
+  this.result = 0;
+}
