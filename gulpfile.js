0ab7bf6198d0e1d7e1ff08693cd450efd15a22d4
Miguel <miguelmail2006@gmail.com>
2019-07-04 19:21:53 -0300

    feature/gulp-automation - Refactor on glup, added scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,48 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
+const GulpFunctions = require('./gulp-functions');
+const sass = require('gulp-sass');
+const teddy = require('gulp-teddy');
+const teddyConf = {
+  setTemplateRoot: ''
+};
 
-/**
- * Task()
- * @param taskname: string
- * @param callback: function
- *
- * OLD Version gulp.task('nombre', ['dependencia1', 'dependencia2'], callback) @return void
- * NEW Version gulp.task('nombre', callback) @return void
- * gulp.series(['d1', 'd2'], callback) @return function
- * gulp.parallel(['d1', 'd2'], callback) @return function
- */
-gulp.task('test', function (done) {
-  console.log('Task test is running');
-  done();
-});
-
-gulp.task('build:dev', function () {
-  // pipeline''
-  /*
-  var nombre = "Miguel"
-  var nombres = ["Miguel", "Gaston", "Alejandro"];
-   */
+gulp.task('build-html:dev', function () {
   return gulp
     .src('./src/**/*.html')
     .pipe(gulp.dest('./.temp'));
 });
 
+gulp.task('build-sass:dev', function () {
+  return gulp
+    .src('./src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./.temp/css'));
+});
+
+gulp.task('build-statics:dev', function () {
+  return gulp
+    .src([
+      './src/**/*.jpg',
+      './src/**/*.jpeg',
+      './src/**/*.png',
+      './src/**/*.gif',
+    ])
+    .pipe(gulp.dest('./.temp/statics/images'));
+});
+
+gulp.task('build:dev', gulp.parallel(['build-html:dev', 'build-sass:dev', 'build-statics:dev']));
+
 gulp.task('watch', gulp.series(['build:dev'], function (done) {
   gulp.watch('./src/**/*.html', gulp.series(['build:dev'])).on('change', browserSync.reload);
+  gulp.watch('./src/scss/**/*.scss', gulp.series(['build:dev'])).on('change', browserSync.reload);
   done();
 }));
 
-gulp.task('serve', function () {
+gulp.task('serve', gulp.series(['watch'], function () {
   browserSync({
     server: {
       baseDir: ['.temp']
     }
   });
-});
+}));
